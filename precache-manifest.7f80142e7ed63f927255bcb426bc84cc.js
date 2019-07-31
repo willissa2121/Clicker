1611ae938289b0ded8452c137b20ef8cf64c27df
Scott Willis <scott.willis.wsu@gmail.com>
2019-07-30 17:31:04 -0700

    Updates

---
+++ precache-manifest.7f80142e7ed63f927255bcb426bc84cc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017d5807b14631d298f4a22c625e791a",
+    "url": "/Clicky/index.html"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "345d30f2c8586a9e7737",
+    "url": "/Clicky/static/js/main.f5c888f1.chunk.js"
+  },
+  {
+    "revision": "1726ed0b9997b87a72d7",
+    "url": "/Clicky/static/js/runtime~main.a36ce3a7.js"
+  }
+]);
